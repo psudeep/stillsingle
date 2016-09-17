18f4f58e8a84a632c2044210febd7e4139f2d1ac
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-09-17 15:09:24 +0530

    coming soon

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,15 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About | Still Single - Mingle Here' });
 });
+router.get('/privacy', function(req, res, next) {
+  res.render('soon', { title: 'Privacy | Still Single - Coming Soon' });
+});
+router.get('/terms', function(req, res, next) {
+  res.render('soon', { title: 'Terms | Still Single - Coming Soon' });
+});
+router.get('/help', function(req, res, next) {
+  res.render('soon', { title: 'FAQs | Still Single - Coming Soon' });
+});
 
 router.get('/restApi/get', RestController.makeGetCalls);
 
